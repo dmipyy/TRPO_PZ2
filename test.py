beb02488968d53f869f9a603549ff412ed8d2b8a
dmipyy <reyper2014@yandex.ru>
2024-12-04 18:09:43 +0300

    fix tests

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class TestCalculator(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add(self):
-        self.assertEqual(self.calc.add(2, 3), 6)
+        self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
 
     def test_subtract(self):
@@ -18,7 +18,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(-2, 3), -6)
 
     def test_divide(self):
-        self.assertEqual(self.calc.divide(6, 2), 2)
+        self.assertEqual(self.calc.divide(6, 2), 3)
         with self.assertRaises(ZeroDivisionError):
             self.calc.divide(5, 0)
 
